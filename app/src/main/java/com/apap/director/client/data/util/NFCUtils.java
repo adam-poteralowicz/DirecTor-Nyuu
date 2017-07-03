c2746b8ec8b069c9209553ce6a1d0a0b40ef3305
Adam Poterałowicz <181710@student.pwr.edu.pl>
2017-07-03 02:48:50 +0200

    NFC Utils rewritten

--- app/src/main/java/com/apap/director/client/data/util/NFCUtils.java
+++ app/src/main/java/com/apap/director/client/data/util/NFCUtils.java
@@ -1,18 +1,11 @@
 package com.apap.director.client.data.util;
 
 import android.content.Intent;
-import android.nfc.FormatException;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
-import android.nfc.Tag;
-import android.nfc.tech.Ndef;
-import android.nfc.tech.NdefFormatable;
 import android.os.Parcelable;
-import android.text.TextUtils;
-import android.util.Log;
 
-import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,40 +20,15 @@ public final class NFCUtils {
         return new NdefMessage(new NdefRecord[]{getNewRecord(mimeType, payload)});
     }
 
-    public static boolean writeMessageToTag(NdefMessage message, Tag tag) {
-        int size = message.toByteArray().length;
-        try {
-            Ndef ndef = Ndef.get(tag);
-            if (ndef != null) {
-                ndef.connect();
-                if (!ndef.isWritable()) {
-                    Log.e(NFCUtils.class.toString(), "Error : Tag is not writable");
-                    return false;
-                }
-                if (ndef.getMaxSize() < size) {
-                    Log.e(NFCUtils.class.toString(), "Error : MessageEntity exceeds the max tag size " + ndef.getMaxSize());
-                    return false;
-                }
-                ndef.writeNdefMessage(message);
-                return true;
-            } else {
-                return provideNdefOperations(tag, message);
-            }
-        } catch (Exception e) {
-            Log.getStackTraceString(e);
-            return false;
-        }
-    }
-
     public static List<String> getStringsFromNfcIntent(Intent intent) {
-        List<String> payloadStrings = new ArrayList<String>();
+        List<String> payloadStrings = new ArrayList<>();
 
         for (NdefMessage message : getMessagesFromIntent(intent)) {
             for (NdefRecord record : message.getRecords()) {
                 byte[] payload = record.getPayload();
                 String payloadString = new String(payload);
 
-                if (!TextUtils.isEmpty(payloadString))
+                if (payloadString.isEmpty())
                     payloadStrings.add(payloadString);
             }
         }
@@ -72,22 +40,7 @@ public final class NFCUtils {
         String action = intent.getAction();
 
         if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action) || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
-            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-            if (rawMsgs == null) {
-                byte[] empty = new byte[]{};
-                final NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, empty, empty);
-                final NdefMessage msg = new NdefMessage(new NdefRecord[]{record});
-                intentMessages = new ArrayList<>();
-                intentMessages.add(msg);
-
-                return intentMessages;
-            }
-
-            for (Parcelable msg : rawMsgs) {
-                if (msg instanceof NdefMessage) {
-                    intentMessages.add((NdefMessage) msg);
-                }
-            }
+            return getMessagesFromNfcTag(intent, intentMessages);
         }
         return intentMessages;
     }
@@ -97,20 +50,34 @@ public final class NFCUtils {
         return new NdefRecord(NdefRecord.TNF_MIME_MEDIA, mimeBytes, new byte[0], payload);
     }
 
-    private static boolean provideNdefOperations(Tag tag, NdefMessage message) {
-        NdefFormatable format = NdefFormatable.get(tag);
-        if (format != null) {
-            try {
-                format.connect();
-                format.format(message);
-                return true;
-            } catch (IOException | FormatException e) {
-                Log.getStackTraceString(e);
-                return false;
-            }
-        } else {
-            Log.e(NFCUtils.class.toString(), "Error : Undefined format");
-            return false;
+    private static NdefMessage constructNdefMessageFromRecords() {
+        byte[] empty = new byte[]{};
+        final NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, empty, empty);
+        return new NdefMessage(new NdefRecord[]{record});
+    }
+
+    /**
+     *
+     * If NFC tag contains NDEF messages, return them.
+     * If not, construct NDEF messages from smaller chunks (NDEF records)
+     */
+    private static List<NdefMessage> getMessagesFromNfcTag(Intent intent, List<NdefMessage> intentMessages) {
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        List<NdefMessage> constructedMessages;
+        if (rawMsgs == null) {
+            final NdefMessage msg = constructNdefMessageFromRecords();
+            constructedMessages = new ArrayList<>();
+            constructedMessages.add(msg);
+
+            return constructedMessages;
         }
+
+        for (Parcelable msg : rawMsgs) {
+            if (msg instanceof NdefMessage) {
+                intentMessages.add((NdefMessage) msg);
+            }
+        }
+
+        return intentMessages;
     }
 }
