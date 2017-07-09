572eeba0599b5da1843f9410aa9c32ee92a2b2d6
Adam Poterałowicz <181710@student.pwr.edu.pl>
2017-07-09 17:25:43 +0200

    Improved LoginPresenter, NewMsgPresenter

--- app/src/main/java/com/apap/director/client/presentation/ui/login/presenter/LoginPresenter.java
+++ app/src/main/java/com/apap/director/client/presentation/ui/login/presenter/LoginPresenter.java
@@ -1,50 +1,37 @@
 package com.apap.director.client.presentation.ui.login.presenter;
 
-import com.apap.director.client.data.db.entity.AccountEntity;
-import com.apap.director.client.domain.interactor.base.Callback;
 import com.apap.director.client.domain.interactor.login.GetAccountListInteractor;
-import com.apap.director.client.domain.repository.AccountRepository;
-import com.apap.director.client.presentation.ui.base.contract.presenter.BasePresenter;
 import com.apap.director.client.presentation.ui.login.contract.LoginContract;
-
-import java.lang.ref.WeakReference;
-import java.util.List;
 
 import javax.inject.Inject;
 
-import io.reactivex.annotations.NonNull;
-import io.reactivex.functions.Consumer;
-import rx.subscriptions.CompositeSubscription;
+import io.reactivex.disposables.CompositeDisposable;
 
 
 public class LoginPresenter implements LoginContract.Presenter {
 
     private LoginContract.View view;
     private GetAccountListInteractor getAccountListInteractor;
+    private CompositeDisposable subscriptions;
 
     @Inject
     public LoginPresenter(LoginContract.View view, GetAccountListInteractor getAccountListInteractor) {
         this.view = view;
         this.getAccountListInteractor = getAccountListInteractor;
+
+        subscriptions = new CompositeDisposable();
     }
 
     @Override
     public void dispose() {
-        getAccountListInteractor.dispose();
+        subscriptions.dispose();
+        subscriptions.clear();
     }
 
     @Override
     public void getAccountList() {
-        getAccountListInteractor.execute(null, new Callback<List<AccountEntity>>() {
-            @Override
-            public void onAccept(List<AccountEntity> data) {
-                view.refreshAccountList(data);
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-                view.handleException(throwable);
-            }
-        });
+        subscriptions.add(getAccountListInteractor.execute(null)
+                .subscribe(data -> view.refreshAccountList(data),
+                        throwable -> view.handleException(throwable)));
     }
 }
